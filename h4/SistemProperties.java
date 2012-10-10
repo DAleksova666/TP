1f92b3307b6350f76042ed0d837bea957b7ac9ef
DAleksova666 <daniela.aleksova95@abv.bg>
2012-10-10 19:55:02 +0300

    initial commit

---
+++ h4/SistemProperties.java
@@ -0,0 +1,53 @@
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+public class SistemProperties{
+
+ public static void main(String[] args){
+  
+
+
+  List<String> output = new ArrayList<String>();
+  
+
+  for(String str:args){
+     
+	if(System.getProperty(str)!=null){
+   
+	output.add(System.getProperty(str));
+     
+`	}   
+  }
+  
+  String[] result = new String[output.size()];
+  output.toArray(result);
+
+   
+
+   int final = args.length-1;
+  
+   if(args[final].equals("up")){
+   Arrays.sort(result);
+   System.out.println(); 
+	for(String str:result){
+	System.out.println(str); 
+        }
+   }
+  
+    if(args[final].equals("down")){
+     Arrays.sort(result,Collections.reverseOrder());
+     System.out.println(); 
+		for(String str:result){
+		System.out.println(str); 
+       		}
+    }
+
+  
+  
+ }
+}
